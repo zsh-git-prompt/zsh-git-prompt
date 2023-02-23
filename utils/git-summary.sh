bc554163ac3baa9d7058b6d3a4fe4a74c3f25ea8
tre <thomas.rebele@orchestranetworks.com>
2023-02-23 10:36:05 +0100

    git-summary.sh: add --ignore-stash and --ignore-defaults

--- utils/git-summary.sh
+++ utils/git-summary.sh
@@ -12,6 +12,16 @@ fi
 
 # unset variables
 unset GIT_SUMMARY_MAX_ENTRIES
+unset GIT_SUMMARY_IGNORE
+GIT_SUMMARY_IGNORE_ACTIVATE=false
+
+declare -A GIT_SUMMARY_IGNORE
+GIT_SUMMARY_IGNORE[REPO_IS_REPOSITORY]=true
+
+ignore_defaults() {
+	GIT_SUMMARY_IGNORE[REPO_BRANCH]=true
+	GIT_SUMMARY_IGNORE[REPO_UPSTREAM]=true
+}
 
 # command line arguments
 while :; do
@@ -19,6 +29,17 @@ while :; do
 		-n)
 			shift
 			GIT_SUMMARY_MAX_ENTRIES="$1"
+			;;
+
+		--ignore-stash)
+			GIT_SUMMARY_IGNORE_ACTIVATE=true
+			GIT_SUMMARY_IGNORE[REPO_STASHED]=true
+			ignore_defaults
+			;;
+
+		--ignore-defaults)
+			GIT_SUMMARY_IGNORE_ACTIVATE=true
+			ignore_defaults
 			;;
 
 		-?*)
@@ -33,7 +54,6 @@ done
 
 # default values
 GIT_SUMMARY_MAX_ENTRIES=${GIT_SUMMARY_MAX_ENTRIES:-5}
-
 
 git_summary_candidates() {
 	z | sort -r -g | awk '{print $2}'
@@ -59,8 +79,25 @@ git_summary_candidates() {
 		# check whether it is dirty
 		cd "$git_dir"
 		stat=$(repo_status)
+		
 		ret="$?"
 		[[ $ret = 0 ]] && continue
+
+		if $GIT_SUMMARY_IGNORE_ACTIVATE; then
+			# check repo status
+			local showRepo=false
+			for line in $(set | grep "^REPO"); do
+				# excluding lines starting with a key from GIT_SUMMARY_IGNORE 
+				local attr=${line%%=*}
+				[[ ${GIT_SUMMARY_IGNORE[$attr]+_} ]] && continue
+				# if value is 0, nothing interesting here
+				local val=${line##*=}
+				[[ "$val" == "0" ]] && continue
+				# 
+				showRepo=true
+			done
+			$showRepo || continue
+		fi
 
 		# print status
 		printf "  %s\t%s\n" ${git_dir//$'\n'/} $stat
